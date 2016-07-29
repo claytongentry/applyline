b891a1b59788deb767a8c9ddab8b477025206ee6
claytongentry <claytongentry2017@u.northwestern.edu>
2016-07-28 17:21:54 -0700

    add users routes

--- lyra/web/router.ex
+++ lyra/web/router.ex
@@ -8,6 +8,11 @@ defmodule Lyra.Router do
   scope "/", Lyra do
     pipe_through :api # Use the default browser stack
 
+    get "/users/:id/tasks", UserController, :with_tasks
+    get "/users/:id/schools", UserController, :with_schools
+    get "/users/:id", UserController, :show
+    get "/users", UserController, :index
+
     get "/tasks/:id", TaskController, :show
     get "/tasks", TaskController, :index
 
